709e42ba4d4dc4596054a5ba119815e35ebbae37
yamisoto <ademoye1@gmail.com>
2024-10-16 12:55:50 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'CGTLX12moye', usernameVariable: 'vmcgtlx')]) {
         sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
         sh 'docker push nanatwn/demo-app:jma-2.0'
